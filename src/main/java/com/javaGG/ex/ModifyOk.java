1f832bffd14bbbbfe4df57fc7348ac119ef490e2
GoRyeoIn <89904365+znzty15@users.noreply.github.com>
2021-09-23 15:29:54 +0900

    password check+

--- src/main/java/com/javaGG/ex/ModifyOk.java
+++ src/main/java/com/javaGG/ex/ModifyOk.java
@@ -18,88 +18,93 @@ import javax.servlet.http.HttpSession;
 
 @WebServlet("/ModifyOk")
 public class ModifyOk extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+   private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	private Connection connection;
-	private Statement stmt;
+   /**
+    * @see HttpServlet#HttpServlet()
+    */
+   private Connection connection;
+   private Statement stmt;
 
-	private String name,id,pw,ph1,ph2,ph3,gender;
+   private String name,id,pw,pw2,ph1,ph2,ph3,gender;
 
-	HttpSession httpsession;
+   HttpSession httpsession;
 
-	public ModifyOk() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+   public ModifyOk() {
+      super();
+      // TODO Auto-generated constructor stub
+   }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		actionDo(request, response);
-	}
+   /**
+    * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+    */
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      // TODO Auto-generated method stub
+      //response.getWriter().append("Served at: ").append(request.getContextPath());
+      actionDo(request, response);
+   }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//doGet(request, response);
-		actionDo(request, response);
-	}
+   /**
+    * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+    */
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      // TODO Auto-generated method stub
+      //doGet(request, response);
+      actionDo(request, response);
+   }
 
-	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+   private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      // TODO Auto-generated method stub
 
-		//한글처리
-		request.setCharacterEncoding("EUC-KR");
-		// 겟세션으로 현재값을 불러오는 과정 필요
-		httpsession = request.getSession();
-		//파라미터
-		name = request.getParameter("name");
-		//id = request.getParameter("id");
-		id = (String)httpsession.getAttribute("id");
-		pw = request.getParameter("pw");
-		ph1 = request.getParameter("ph1");
-		ph2 = request.getParameter("ph2");
-		ph3 = request.getParameter("ph3");
-		gender = request.getParameter("gender");
-		String query = "update members set name='" + name + "', ph1 = '" + ph1 + "', p2 = '" + ph2 + "', ph3 = '" + ph3 + "', gender='" + gender + "' where id='" + id + "'";
-		//try catch 문
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","scott","tiger");
-			stmt = connection.createStatement();
-
-			int i  = stmt.executeUpdate(query);  // query가 성공하면 1, 실패하면 0
-
-			if(i==1) {
-				System.out.println("정보수정 성공"); // 콘솔창에 찍어주기
-				httpsession.setAttribute("name", name);
-				response.sendRedirect("modifyResult.jsp"); // 정보수정 성공하면 modifyResult.jsp 페이지로 이동
-			} else {
-				System.out.println("정보수정 실패"); // 콘솔창에 찍어주기
-				response.sendRedirect("modify.jsp"); // 정보수정 실패하면 modify.jsp로 이동
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-
-				if(stmt != null) stmt.close();
-				if(connection != null) connection.close();
-
-			}catch (Exception e) {
-				e.printStackTrace();
-			}
-
-		}
-	}
+      //한글처리
+      request.setCharacterEncoding("EUC-KR");
+      httpsession = request.getSession(); // 겟세션으로 현재값을 불러오는 과정이 필요하다!
+      //파라미터어
+      name = request.getParameter("name");
+      //id = request.getParameter("id");
+      id = (String)httpsession.getAttribute("id");
+      pw = request.getParameter("pw");
+      pw2 = (String)httpsession.getAttribute("pw"); 
+      ph1 = request.getParameter("ph1");
+      ph2 = request.getParameter("ph2");
+      ph3 = request.getParameter("ph3");
+      gender = request.getParameter("gender");
+      
+      if(httpsession.getAttribute("pw").equals(pw)) {
+	      String query =    "update members set name='" + name + "', ph1='" + ph1 + "', ph2='" + ph2 + "', ph3='" + ph3 + "', gender='" + gender + "' where id='" + id + "' ";
+	
+	      //try catch 문
+	      try {
+	         Class.forName("oracle.jdbc.driver.OracleDriver");
+	         connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","scott","tiger");
+	         stmt = connection.createStatement();
+	
+	         int i  = stmt.executeUpdate(query); // query 성공하면 1, 실패하면 0
+	
+	         if(i==1) {
+	            System.out.println("정보수정 성공"); // 콘솔창에 찍어주기
+	            httpsession.setAttribute("name", name); //수정된 이름을 세션에 overwrite
+	            response.sendRedirect("modifyResult.jsp"); // 정보수정 성공 시 modifyResult.jsp로 이동
+	         } else {
+	            System.out.println("정보수정 실패"); // 콘솔창에 찍어주기
+	            response.sendRedirect("modify.jsp"); // 정보수정에 실패하면 modify.jsp로 이동하여 다시 정보 입력
+	         }
+	
+	      } catch (Exception e) {
+	         e.printStackTrace();
+	      } finally {
+	         try {
+	            if(stmt != null) stmt.close();
+	            if(connection != null) connection.close();
+	         }catch (Exception e) {
+	            e.printStackTrace();
+	         }
+	
+	      }
+      }else {
+    	  System.out.println("비밀번호가 틀립니다.");
+    	  response.sendRedirect("modify.jsp");
+      }
+   }
 
 }
